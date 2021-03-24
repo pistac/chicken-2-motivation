46010cc17f2d82dfda19c854def7a9a5f2064ada
pistac <ilaria.torre@cognovo.eu>
2021-03-24 12:10:58 +0100

    Initial commit

---
+++ Chicken/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,217 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Controls player input and movement. Also checks for game over conditions.
+public class PlayerController : MonoBehaviour {
+
+#pragma warning disable
+	[SerializeField]
+	private SharedVariableManager sharedVariableManager;
+	[SerializeField]
+	private Transform robotTransform;
+	[SerializeField]
+	private float gameOverDelay = 3.0f;
+	[SerializeField]
+	private float collisionDelay = 0.5f;
+#pragma warning restore
+
+	private bool currentlySwerving = false;
+	private bool paused = true;
+	private float margin;
+	private float speed;
+	private float swerveForwardSpeed;
+	private float swerveSideSpeed;
+	private float rotationSpeed;
+	private float radius;
+	private int swerveDirection = 0;
+	private Vector3 playerDirection;
+	private Vector3 positiveSwerveDirection;
+	private Vector3 upDirection;
+
+	void OnEnable() {
+		// Subscribe unpausing to when the loading screen is finished.
+		SharedVariableManager.onLoadIsFinished += UnPause;
+	}
+
+	void OnDisable() {
+		// Mandatory unsibscription.
+		SharedVariableManager.onLoadIsFinished -= UnPause;
+	}
+
+	void Awake() {
+		// Calculate movement basis vectors relative to parent.
+		playerDirection = transform.parent.InverseTransformDirection(transform.forward);
+		positiveSwerveDirection = transform.parent.InverseTransformDirection(transform.right);
+
+		speed = sharedVariableManager.agentSpeed;
+		swerveForwardSpeed = speed * (1 - sharedVariableManager.swerveSideSpeedRatio);
+		swerveSideSpeed = speed * sharedVariableManager.swerveSideSpeedRatio;
+		rotationSpeed = sharedVariableManager.rotationSpeed;
+		radius = GetComponent<CapsuleCollider>().radius;
+		margin = sharedVariableManager.swerveMargin; // The swerving margin is a part of the width.
+		sharedVariableManager.playerRadius = radius;
+	}
+
+	public void Pause() {
+		paused = true;
+	}
+
+	public void UnPause() {
+		paused = false;
+	}
+
+	void Update() {
+		// Only execute if not paused.
+		if (paused) return;
+
+		// Check if player has swerved fully.
+		if (Mathf.Abs(transform.localPosition.x) >= sharedVariableManager.swerveWidthOfLargestAgent) {
+			sharedVariableManager.playerHasSwerved = true;
+			currentlySwerving = false;
+		}
+
+		// Check if experiment is over.
+		if (ExperimentIsOver() && !sharedVariableManager.gameIsOver) {
+			// Commence game over sequence.
+			StartCoroutine(GameOverSequence());
+		}
+	}
+
+	// Waits for a specified time period, then sets the gameIsOver flag.
+	IEnumerator GameOverSequence() {
+		// Wait for the game over delay.
+		yield return new WaitForSecondsRealtime(gameOverDelay);
+
+		// Pause the movement and animation of robot and player avatars.
+		Pause();
+		RobotController robot = FindObjectOfType<RobotController>();
+		robot.Pause();
+
+		Animator playerAnimator = gameObject.GetComponentInChildren<Animator>();
+		Animator robotAnimator = robot.gameObject.GetComponentInChildren<Animator>();
+
+		if (playerAnimator != null) {
+			playerAnimator.SetTrigger("idleStand");
+		}
+
+		if (robotAnimator != null && robotAnimator.gameObject.name.Contains("Sophia")) {
+			robotAnimator.SetTrigger("idleStand");
+		}
+
+		// Signal that the game is over.
+		sharedVariableManager.gameIsOver = true;
+	}
+
+	void FixedUpdate() {
+		// Only execute if not paused.
+		if (paused) return;
+
+		// Handle the movement of the player.
+		MovePlayer();
+	}
+
+	// Take player input and moves the player forward, swerving according to input.
+	// Rotation is also controlled by this method.
+	private void MovePlayer() {
+		// Check for swerving input.
+		bool swerveInput = Input.GetKey(KeyCode.Space);
+
+		// If there is swerve input and player is not currently swerving.
+		if (swerveInput && !currentlySwerving) {
+			sharedVariableManager.playerSwerved = true;
+
+			// Report swerve distances.
+			sharedVariableManager.playerRobotSwerveDistance = Mathf.Abs((robotTransform.position - transform.position).z);
+			sharedVariableManager.playerStartSwerveDistance = Mathf.Abs((GameObject.Find("PlayerStartPoint").transform.position - transform.position).z);
+
+			// Pick a random swerve direction.
+			PickSwerveDirection();
+			currentlySwerving = true;
+		}
+
+		// If the player is not currently swerving or has already swerved.
+		if (!currentlySwerving || sharedVariableManager.playerHasSwerved) {
+			// Move player in playerDirection at speed.
+			transform.localPosition += playerDirection * speed * Time.fixedDeltaTime;
+
+			// Rotate player towards playerDirection.
+			transform.localRotation = Quaternion.LookRotation(
+				Vector3.RotateTowards(transform.parent.InverseTransformDirection(transform.forward),
+				playerDirection, rotationSpeed, 0.0f));
+		} else { // If the player is currently swerving and has not yet swerved fully.
+				 // Move player along swerve direction.
+				 // Vector3 direction = Vector3.Normalize(playerDirection + (positiveSwerveDirection * swerveDirection));
+				 // transform.localPosition += direction * swerveSpeed * Time.fixedDeltaTime;
+			Vector3 swerveVelocity = swerveForwardSpeed * playerDirection + swerveSideSpeed * positiveSwerveDirection * swerveDirection;
+			transform.localPosition += swerveVelocity * Time.fixedDeltaTime;
+
+			// Rotate player towards the swerve direction.
+			transform.localRotation = Quaternion.LookRotation(
+				Vector3.RotateTowards(transform.parent.InverseTransformDirection(transform.forward),
+				swerveVelocity.normalized, rotationSpeed, 0.0f));
+		}
+	}
+
+	// Checks for collisions between the player and the robot.
+	private void OnCollisionEnter(Collision other) {
+		if (other.gameObject.tag == "Robot" && !sharedVariableManager.collisionHasHappened) {
+			sharedVariableManager.collisionHasHappened = true;
+			if (Application.isEditor) Debug.Log("collided");
+			StartCoroutine(PauseAfterCollision());
+		}
+	}
+
+	IEnumerator PauseAfterCollision() {
+		Pause();
+		RobotController robot = FindObjectOfType<RobotController>();
+		robot.Pause();
+
+		Animator playerAnimator = gameObject.GetComponentInChildren<Animator>();
+		Animator robotAnimator = robot.gameObject.GetComponentInChildren<Animator>();
+
+		if (playerAnimator != null) {
+			playerAnimator.SetTrigger("getHitMiddle");
+		}
+
+		if (robotAnimator != null && robotAnimator.gameObject.name.Contains("Sophia")) {
+			robotAnimator.SetTrigger("getHitMiddle");
+		}
+
+		yield return new WaitForSecondsRealtime(collisionDelay);
+
+		if (playerAnimator != null) {
+			playerAnimator.SetTrigger("walk");
+		}
+
+		if (robotAnimator != null && robotAnimator.gameObject.name.Contains("Sophia")) {
+			robotAnimator.SetTrigger("walk");
+		}
+
+		UnPause();
+		robot.UnPause();
+		robot.OverrideSwerveDistance();
+	}
+
+	// Checks if the condition for the experiment to be over has been reached.
+	// In this case, the experiment is over if the robot has passed the
+	private bool ExperimentIsOver() {
+		return robotTransform.localPosition.z <= transform.localPosition.z ||
+			  sharedVariableManager.collisionHasHappened;
+	}
+
+	// Picks and sets the swerve direction to be away from the robot's swerved direction.
+	// If robot has not yet swerved, pick uniformly random direction.
+	private void PickSwerveDirection() {
+		float robotSwerveAmount = GameObject.Find("RobotAgent").transform.localPosition.x;
+		// If player has started swerving to their right.
+		if (robotSwerveAmount < 0) {
+			swerveDirection = 1; // Swerve right.
+		} else if (robotSwerveAmount > 0) { // If player has started swerving to their left.
+			swerveDirection = -1; // Swerve left.
+		} else { // If player has not swerved at all.
+				 // Pick randomly to swerve left or right.
+			swerveDirection = Random.Range(0, 2) == 0 ? -1 : 1;
+		}
+	}
+}
---
+++ Chicken/Assets/Scripts/RobotController.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Controls robot movement.
+public class RobotController : MonoBehaviour {
+
+#pragma warning disable
+	[SerializeField]
+	private SharedVariableManager sharedVariableManager;
+	[SerializeField]
+	private Transform playerTransform;
+	[SerializeField]
+	private bool debugDoNotSwerve = false;
+#pragma warning restore
+
+	private bool paused = true;
+	private bool robotHasSwerved = false;
+	private float margin;
+	private float swerveDistance;
+	private float speed;
+	private float swerveForwardSpeed;
+	private float swerveSideSpeed;
+	private float rotationSpeed;
+	private float radius;
+	private int swerveDirection = 0;
+
+	private Vector3 positiveSwerveDirection;
+	private Vector3 robotDirection;
+
+	void OnEnable() {
+		// Subscribe unpausing to when the loading screen is finished.
+		SharedVariableManager.onLoadIsFinished += UnPause;
+	}
+
+	void OnDisable() {
+		// Mandatory unsibscription.
+		SharedVariableManager.onLoadIsFinished -= UnPause;
+	}
+
+	public void OverrideSwerveDistance() {
+		swerveDistance = Mathf.Infinity;
+	}
+
+	public void UpdateWidth() {
+		radius = GetComponent<CapsuleCollider>().radius;
+		margin = sharedVariableManager.swerveMargin;
+		sharedVariableManager.robotRadius = radius;
+
+		// Minimum distance is calculated as the distance away from the player the robot
+		// can be and still have time to swerve away assuming the player does not swerve.
+		// 0.70710678118 is sin(45deg), sharedVariableManager.swerveSideSpeedRatio is always 0.5f.
+		float minimumDistance = (sharedVariableManager.robotRadius + sharedVariableManager.playerRadius + sharedVariableManager.swerveMargin) * 0.70710678118f * ((2 - sharedVariableManager.swerveSideSpeedRatio) / sharedVariableManager.swerveSideSpeedRatio + 1);
+		// An error margin is added to the minimum distance in order to have differen behavior each time.
+		// The error margin is skewed depending on the motivation the robot has.
+		// If the trial manager is not instanced yet, the pur Gaussian is used.
+		TrialManager trialManager = TrialManager.instance;
+		if (trialManager != null && trialManager.trials != null) {
+			swerveDistance = HelperFunctions.CalculateSwerveDistance(minimumDistance, sharedVariableManager.swerveStandardDeviation, TrialManager.instance.trials[TrialManager.instance.currentTrialNum].robotMotivation);
+		} else {
+			swerveDistance = HelperFunctions.GenerateGaussian(minimumDistance, sharedVariableManager.swerveStandardDeviation);
+		}
+		
+		if (Application.isEditor) {
+			if (debugDoNotSwerve) {
+				swerveDistance = -10.0f;
+			}
+		}
+	}
+
+	void Awake() {
+		robotDirection = transform.parent.InverseTransformDirection(transform.forward);
+		positiveSwerveDirection = transform.parent.InverseTransformDirection(transform.right);
+		speed = sharedVariableManager.agentSpeed;
+		swerveForwardSpeed = speed * (1 - sharedVariableManager.swerveSideSpeedRatio);
+		swerveSideSpeed = speed * sharedVariableManager.swerveSideSpeedRatio;
+		rotationSpeed = sharedVariableManager.rotationSpeed;
+	}
+
+	void Start() {
+		UpdateWidth();
+	}
+
+	public void Pause() {
+		paused = true;
+	}
+
+	public void UnPause() {
+		paused = false;
+	}
+
+	void Update() {
+		// Only execute if not paused.
+		if (paused) return;
+		// Check if robot has swerved fully.
+		if (Mathf.Abs(transform.localPosition.x) >= sharedVariableManager.swerveWidthOfLargestAgent) {
+			robotHasSwerved = true;
+		}
+	}
+
+	void FixedUpdate() {
+		// Only execute if not paused.
+		if (paused) return;
+
+		// Handle the movement of the robot.
+		MoveRobot();
+	}
+
+	// Moves the robot forward, swerving according to player position.
+	// Rotation is also controlled by this method.
+	private void MoveRobot() {
+		float playerRobotDistance = Vector3.Distance(playerTransform.localPosition, transform.localPosition);
+
+		// If the player is beyond the minimum swerve distance or has already swerved.
+		// Also if the robot has swerved fully. Move normally.
+		if (playerRobotDistance > swerveDistance ||
+			sharedVariableManager.playerHasSwerved ||
+			robotHasSwerved) {
+			// Move robot in robotDirection at speed.
+			transform.localPosition += robotDirection * speed * Time.fixedDeltaTime;
+
+			// Rotate robot.
+			transform.localRotation = Quaternion.LookRotation(
+				Vector3.RotateTowards(transform.parent.InverseTransformDirection(transform.forward),
+				robotDirection, rotationSpeed, 0.0f));
+		} else { // If the player is within the minimum swerve distance.
+				 // Pick the appropriate swerve direction if none has been picked, and report distances.
+			if (swerveDirection == 0) {
+				PickSwerveDirection();
+
+				sharedVariableManager.robotSwerved = true;
+
+				// Report swerve distances.
+				sharedVariableManager.robotPlayerSwerveDistance = Mathf.Abs((playerTransform.position - transform.position).z);
+				sharedVariableManager.robotStartSwerveDistance = Mathf.Abs((GameObject.Find("RobotStartPoint").transform.position - transform.position).z);
+			}
+
+			// Move robot along swerve direction.
+			// Vector3 direction = Vector3.Normalize(robotDirection + (positiveSwerveDirection * swerveDirection));
+			// transform.localPosition += direction * swerveSpeed * Time.fixedDeltaTime;
+			Vector3 swerveVelocity = swerveForwardSpeed * robotDirection + swerveSideSpeed * positiveSwerveDirection * swerveDirection;
+			transform.localPosition += swerveVelocity * Time.fixedDeltaTime;
+
+			// Rotate robot.
+			transform.localRotation = Quaternion.LookRotation(
+				Vector3.RotateTowards(transform.parent.InverseTransformDirection(transform.forward),
+				swerveVelocity.normalized, rotationSpeed, 0.0f));
+		}
+	}
+
+	// Picks and sets the swerve direction to be away from the player's swerved direction.
+	// If player has not yet swerved, pick uniformly random direction.
+	private void PickSwerveDirection() {
+		float playerSwerveAmount = playerTransform.localPosition.x;
+		// If player has started swerving to their right.
+		if (playerSwerveAmount > 0) {
+			swerveDirection = 1; // Swerve right.
+		} else if (playerSwerveAmount < 0) { // If player has started swerving to their left.
+			swerveDirection = -1; // Swerve left.
+		} else { // If player has not swerved at all.
+				 // Pick randomly to swerve left or right.
+			swerveDirection = Random.Range(0, 2) == 0 ? -1 : 1;
+		}
+	}
+}
